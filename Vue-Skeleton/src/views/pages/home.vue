f93dee501802cb487298c93989a80dec1f92b8c9
Shimona Prabhu <0718shimona@gmail.com>
2022-11-23 15:51:38 -0800

    added night sky

--- Vue-Skeleton/src/views/pages/home.vue
+++ Vue-Skeleton/src/views/pages/home.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <BeeChart v-if="dataExists" :myBeechartData="myBeeData" /> 
+        <NightSky v-if="dataExists" :myNightskyData="myNightskyData" /> 
         <!-- <h1>Welcome Home</h1>
         <BarChart v-if="dataExists" :myBarchartData="myBarData" /> -->
 
@@ -10,6 +11,7 @@
 <script>
 // import BarChart from "../components/barchart.vue"
 import BeeChart from "../components/beeswarm.vue"
+import NightSky from "../components/nightsky.vue"
 import * as d3 from "d3";
 import csvPath from '../../assets/data/SF_Historical_Ballot_Measures.csv';
 
@@ -18,14 +20,18 @@ export default {
         return {
             dataExists: false,
             myBeeData: [],
+            myNightskyData: []
         }
     },
     components: {
-        BeeChart
+        BeeChart,
+        NightSky
     },
     created(){
         /* Fetch via CSV */
-        this.drawBeeFromCsv()
+        this.drawBeeFromCsv(),
+        this.drawNightFromCsv()
+
     },
     mounted(){},
     methods: {
@@ -37,6 +43,17 @@ export default {
                 // console.log(data);
                 this.dataExists = true; // updates the v-if to conditionally show the barchart only if our data is here.
                 this.myBeeData = data; // updates the prop value to be the recieved data, which we hand in to our bar-chart
+
+            });
+        },
+        drawNightFromCsv(){
+            //async method
+            d3.csv(csvPath).then((data) => {
+                // array of objects
+                // console.log(data.length);
+                // console.log(data);
+                this.dataExists = true; // updates the v-if to conditionally show the barchart only if our data is here.
+                this.myNightskyData = data; // updates the prop value to be the recieved data, which we hand in to our bar-chart
 
             });
         }
